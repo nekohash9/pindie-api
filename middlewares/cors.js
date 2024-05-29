9e2f3646991974a432ea81c52b85f09b88a7cd97
nekohash9 <nekonotgay@gmail.com>
2024-05-29 06:17:28 +0300

    udpated cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -4,9 +4,7 @@ const allowedCors = [
   "http://localhost:3000",
   "http://localhost:3001",
   "https://pindie-front-sadovnichy.nomorepartiesco.ru",
-  "http://pindie-front-sadovnichy.nomorepartiesco.ru",
-  "https://pindie-backend-sadovnichy.nomorepartiesco.ru",
-  "http://pindie-backend-sadovnichy.nomorepartiesco.ru",
+  "http://pindie-front-sadovnichy.nomorepartiesco.ru"
 ];
 
 const cors = (req, res, next) => {
